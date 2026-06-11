4903fe48a253d70a9a585adefaa32374114162a6
Mounika S S <mounikass2005@gmail.com>
2026-06-11 13:20:32 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.java
@@ -0,0 +1,46 @@
+class Solution {
+    public void islandsAndTreasure(int[][] grid) {
+
+        Queue<int[]> q = new LinkedList<>();
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 0) {
+                    q.offer(new int[]{r, c});
+                }
+            }
+        }
+
+        int[][] dir = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+        };
+
+        while (!q.isEmpty()) {
+
+            int[] cur = q.poll();
+            int r = cur[0];
+            int c = cur[1];
+
+            for (int[] d : dir) {
+
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr < 0 || nc < 0 ||
+                    nr >= rows || nc >= cols ||
+                    grid[nr][nc] != Integer.MAX_VALUE) {
+                    continue;
+                }
+
+                grid[nr][nc] = grid[r][c] + 1;
+                q.offer(new int[]{nr, nc});
+            }
+        }
+    }
+}
