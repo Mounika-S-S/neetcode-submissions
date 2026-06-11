dccbdfde8f1ef7136d111b9f7f92eceb3a093e85
Mounika S S <mounikass2005@gmail.com>
2026-06-11 18:52:51 +0530

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    int[] par;
+    public boolean validTree(int n, int[][] edges) {
+        //union find-connected components
+        if(edges.length != n - 1)
+            return false;
+        par=new int[n];
+        for(int i=0;i<n;i++){
+            par[i]=i;
+        }
+
+        for(int[] edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            if(find(u)==find(v)) return false;
+            union(u,v);
+        }
+        return true;
+    }
+    public int find(int u){
+        if(u==par[u]) return u;
+        par[u]=find(par[u]);
+        return par[u];
+    }
+    public void union(int u,int v){
+        int pu=find(u);
+        int pv=find(v);
+        if(pu!=pv){
+            par[pu]=pv;
+        }
+    }
+}
